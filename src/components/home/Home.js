9ef42dd765c2315b35c28bc924bcf0b1d099b863
Krish Vishwakarma <kkrish7654@gmail.com>
2023-02-06 00:38:33 +0530

    may fixed

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { useSelector } from 'react-redux'
 import axios from 'axios'
 import { useEffect } from 'react'
@@ -9,27 +9,24 @@ import ProductsCard from './ProductsCard'
 const Home = () => {
     const products = useSelector((state)=>state.allProducts.products)
     const dispatch = useDispatch();
-    const ProductfetchApi = async() => {
+    const ProductfetchApi =  useCallback(async() => {
+      try{
       const response = await axios.get("https://fakestoreapi.com/products")
-      .catch((err)=>console.log(err))
       console.log(response.data)
       dispatch(setProducts(response.data))
+      }catch(err){
+        console.log(err);
+      }
   
-    }
+    })
     useEffect(() => {
       ProductfetchApi();
-    })
+    },[])
 
   return (
     <div>
       <div className='grid grid-cols-2 gap-3'>
-        {
-            products.map((product)=> {
-                return (
-                    <ProductsCard key={product.id}/>
-                )
-            })
-         }
+        <ProductsCard/>
       </div>
     </div>
   )
